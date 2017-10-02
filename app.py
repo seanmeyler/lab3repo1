dfd6d0a5347c2605fc620e013a9d27c8760073e1
Basel Magableh <basel.magableh@icloud.com>
2017-10-02 21:46:39 +0100

    add comments

--- app.py
+++ app.py
@@ -3,18 +3,22 @@ from flask import Flask
 from flask_mysqldb import MySQL
 mysql = MySQL()
 app = Flask(__name__)
+# My SQL Instance configurations 
+# Change the HOST IP and Password to match your instance configurations
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'bassel99'
 app.config['MYSQL_DB'] = 'studentsbook'
 app.config['MYSQL_HOST'] = '35.195.195.114'
 mysql.init_app(app)
- 
+
+# The first route to access the webservice from http://external-ip:5000/ 
+#@pp.route("/add") this will create a new endpoints that can be accessed using http://external-ip:5000/add
 @app.route("/")
-def hello():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM entries''')
-    rv = cur.fetchall()
-    return str(rv)
+def hello(): # Name of the method
+    cur = mysql.connection.cursor() #create a connection to the SQL instance
+    cur.execute('''SELECT * FROM entries''') # execute an SQL statment
+    rv = cur.fetchall() #Retreive all rows returend by the SQL statment
+    return str(rv)      #Return the data in a string format
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', port='5000')
+        app.run(host='0.0.0.0', port='5000') #Run the flask app at port 5000
 
